01d2434c27101ebd888ca617350c01ae918afdcf
KojoBarbie <tmkhsy-0420@g.ecc.u-tokyo.ac.jp>
2022-12-29 17:56:29 +0900

    221229

--- tessoku-book/a03/main.py
+++ tessoku-book/a03/main.py
@@ -1,9 +1,23 @@
 """
+赤いカードが N 枚あり、それぞれ整数 P 1,P 2,⋯,P N が書かれています。
+また、青いカードが N 枚あり、それぞれ整数 Q 1,Q 2 ,⋯,Q Nが書かれています。
 
+太郎君は、赤いカードの中から 1 枚、青いカードの中から 1 枚、合計 2 枚のカードを選びます。
+選んだ 2 枚のカードに書かれた整数の合計が K となるようにする方法は存在しますか。
 """
 
 def main():
-    N = map(int, open(0).read().split())
+    N, K = map(int, input().split())
+    array_p = list(map(int, input().split()))
+    array_q = list(map(int, input().split()))
+
+    for p in array_p:
+        for q in array_q:
+            if p + q == K:
+                print("Yes")
+                return
+
+    print("No")
 
 
 main()
