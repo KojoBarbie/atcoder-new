4d881b316bc55f1d3c0326a54d3e0a13102b1ef1
KojoBarbie <tmkhsy-0420@g.ecc.u-tokyo.ac.jp>
2023-01-10 02:46:31 +0900

    20230110

--- tessoku-book/a05/main.py
+++ tessoku-book/a05/main.py
@@ -1,9 +1,20 @@
 """
-
+赤・青・白の 3 枚のカードがあります。
+太郎君は、それぞれのカードに 1 以上 N 以下の整数を書かなければなりません。
+3 枚のカードの合計を K にするような書き方は何通りありますか。
 """
 
 def main():
-    N = map(int, open(0).read().split())
+    N, K = map(int, input().split())
+    count = 0
+
+    for i in range(1, N + 1):
+        for j in range(1, N + 1):
+            num = K - i - j
+            if num >= 1 and num <= N:
+                count += 1
+
+    print(count)
 
 
 main()
--- tessoku-book/a06/main.py
+++ tessoku-book/a06/main.py
@@ -1,9 +1,20 @@
 """
+遊園地「ALGO-RESORT」では N 日間にわたるイベントが開催され、 i 日目 (1≤i≤N) には Ai 人が来場しました。
 
+以下の合計 Q 個の質問に答えるプログラムを作成してください。
+1 個目の質問：L1 日目から R1 日目までの合計来場者数は？
 """
 
 def main():
-    N = map(int, open(0).read().split())
+    N, Q = map(int, input().split())
+    A = list(map(int, input().split()))
+    array = [0]
+    for num in range(N):
+        array.append(A[num] + array[num])
+
+    for i in range(Q):
+        L, R = map(int, input().split())
+        print(array[R] - array[L - 1])
 
 
 main()
