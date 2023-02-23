d91d236d453a644aff54756417c511e6fbed53ab
KojoBarbie <tmkhsy-0420@g.ecc.u-tokyo.ac.jp>
2023-02-23 16:28:13 +0900

    20230223

--- tessoku-book/a07/main.py
+++ tessoku-book/a07/main.py
@@ -3,7 +3,20 @@
 """
 
 def main():
-    N = map(int, open(0).read().split())
+    D = int(input())
+    N = int(input())
+    attendee = [0] * (D + 2)
+
+    # 人数の増減を配列に記録
+    for i in range(N):
+        L, R = map(int, input().split())
+        attendee[L] += 1
+        attendee[R + 1] -= 1
+
+    result = 0
+    for j in range(1, len(attendee) - 1):
+        result += attendee[j]
+        print(result)
 
 
 main()
